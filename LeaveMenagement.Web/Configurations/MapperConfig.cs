1d799adedc42681e7134d187b087e3cdd7e04e24
Davor Burmudžija <davorburmudzija@gmail.com>
2022-07-01 21:53:23 +0200

    Modified and added datatables and swal. End of Leave Type Managment Module

---
+++ LeaveMenagement.Web/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveMenagement.Web.Data;
+using LeaveMenagement.Web.Models;
+
+namespace LeaveMenagement.Web.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
